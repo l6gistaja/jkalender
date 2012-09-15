fcaf7d32c41814459124e4dc97ba7f52e45fb156
Juks <juks@alkohol.ee>
2012-09-15 20:55:54 +0300

    todo: icalendar

--- src/ee/alkohol/juks/sirvid/web/JKalender.java
+++ src/ee/alkohol/juks/sirvid/web/JKalender.java
@@ -3,12 +3,27 @@ package ee.alkohol.juks.sirvid.web;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import ee.alkohol.juks.sirvid.containers.InputData;
 
 public class JKalender extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-      throws ServletException, IOException {
+
+@Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+      InputData inputData = new InputData();
+      inputData.setDate(request.getParameter("date"));
+      inputData.setTimezone(request.getParameter("tz"));
+      inputData.setOutputFormat(request.getParameter("f"));
+      inputData.setUseDynamicTime(request.getParameter("dt"));
+      inputData.setCalculateMoonphases(request.getParameter("mo"));
+      inputData.setCalculateSolistices(request.getParameter("sl"));
+      inputData.setCalculateSunrisesSunsets(request.getParameter("sr"));
+      inputData.setCalculateGregorianEaster(request.getParameter("ge"));
+      inputData.setCalculateJulianEaster(request.getParameter("je"));
+      inputData.setLatitude(request.getParameter("lat"));
+      inputData.setLongitude(request.getParameter("lon"));
+      inputData.setTimespan(request.getParameter("ts"));
+      
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
     String docType =
@@ -18,7 +33,8 @@ public class JKalender extends HttpServlet {
                 "<HTML>\n" +
                 "<HEAD><TITLE>Hello</TITLE></HEAD>\n" +
                 "<BODY BGCOLOR=\"#FDF5E6\">\n" +
-                "<H1>Hello</H1>\n" +
+                "<H1>InputData</H1>\n" +
+                inputData.toString() +
                 "</BODY></HTML>");
   }
 }
